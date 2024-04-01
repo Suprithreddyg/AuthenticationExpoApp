2c07fe389e265db5e5238010125f49cb2b7553c7
Suprithreddyg <86020090+Suprithreddyg@users.noreply.github.com>
2024-03-31 18:14:31 -0600

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAki20ygtrfNhKbaZKI9owJMWWNz4_tsJI",
-  authDomain: "subscriptions-a6823.firebaseapp.com",
-  databaseURL: "https://subscriptions-a6823-default-rtdb.firebaseio.com",
-  projectId: "subscriptions-a6823",
-  storageBucket: "subscriptions-a6823.appspot.com",
-  messagingSenderId: "540048402072",
-  appId: "1:540048402072:web:2e8d5cf08abf4e198e12ed",
-  measurementId: "G-C688QCT210"
+ 
 };
 
 // Initialize Firebase
